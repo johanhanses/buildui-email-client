aaf6a08cb0cda4557fed59b6fdca32aa7edc49e0
Johan Hanses <johanhanses@gmail.com>
2023-01-05 10:56:37 +0100

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import * as Icons from '@heroicons/react/outline'
-import { motion } from 'framer-motion'
+import { AnimatePresence, motion } from 'framer-motion'
 
 let titles = [
   ["Apple's newest iPhone is here", 'Watch our July event'],
@@ -13,14 +13,21 @@ let titles = [
 
 export default function Email() {
   const [messages, setMessages] = useState([...Array(9).keys()])
+  const [selectedMessages, setSelectedMessages] = useState<number[]>([])
+
+  function toggleMessage(mid: number) {
+    if (selectedMessages.includes(mid)) setSelectedMessages((messages) => messages.filter((id) => id !== mid))
+    if (!selectedMessages.includes(mid)) setSelectedMessages((messages) => [...messages, mid])
+  }
 
   function addMessage() {
     const newId = (messages.at(-1) || 0) + 1
     setMessages((messages) => [...messages, newId])
   }
 
-  function archiveMessage(mid: any) {
-    setMessages((messages) => messages.filter((id) => id !== mid))
+  function archiveSelectedMessages() {
+    setMessages((messages) => messages.filter((id) => !selectedMessages.includes(id)))
+    setSelectedMessages([])
   }
 
   return (
@@ -35,36 +42,61 @@ export default function Email() {
               >
                 <Icons.MailIcon className="h-5 w-5 " />
               </button>
+              <button
+                onClick={archiveSelectedMessages}
+                className="-mx-2 rounded px-2 py-1 text-slate-400 hover:text-slate-500 active:bg-slate-200"
+              >
+                <Icons.ArchiveIcon className="h-5 w-5" />
+              </button>
             </div>
           </div>
           <ul className="overflow-y-scroll px-3 pt-2">
-            {[...messages].reverse().map((mid) => (
-              <motion.li
-                initial={{ opacity: 0, height: 0 }}
-                animate={{ opacity: 1, height: 'auto' }}
-                exit={{}}
-                // transition={{ type: 'spring', bounce: 0.5, duration: 1 }}
-                key={mid}
-                className="relative"
-              >
-                <div className="py-0.5">
-                  <button
-                    onClick={() => archiveMessage(mid)}
-                    className="block w-full cursor-pointer truncate rounded py-3 px-3 text-left hover:bg-slate-200"
-                  >
-                    <p className="truncate text-sm font-medium text-slate-500">{titles[mid % titles.length][0]}</p>
-                    <p className="truncate text-xs text-slate-400">{titles[mid % titles.length][1]}</p>
-                  </button>
-                </div>
-              </motion.li>
-            ))}
+            <AnimatePresence initial={false}>
+              {[...messages].reverse().map((mid) => (
+                <motion.li
+                  initial={{ opacity: 0, height: 0 }}
+                  animate={{ opacity: 1, height: 'auto' }}
+                  exit={{ opacity: 0, height: 0 }}
+                  transition={{ opacity: { duration: 0.2 } }}
+                  key={mid}
+                  className="relative"
+                >
+                  <div className="py-0.5">
+                    <button
+                      onClick={() => toggleMessage(mid)}
+                      className={`${
+                        selectedMessages.includes(mid) ? 'bg-blue-500' : 'hover:bg-slate-200'
+                      } block w-full cursor-pointer truncate rounded py-3 px-3 text-left`}
+                    >
+                      <p
+                        className={`${
+                          selectedMessages.includes(mid) ? 'text-white' : 'text-slate-500'
+                        } font-mediu truncate text-sm`}
+                      >
+                        {titles[mid % titles.length][0]}
+                      </p>
+                      <p
+                        className={`${
+                          selectedMessages.includes(mid) ? 'text-blue-200' : 'text-slate-400'
+                        } truncate text-xs`}
+                      >
+                        {titles[mid % titles.length][1]}
+                      </p>
+                    </button>
+                  </div>
+                </motion.li>
+              ))}
+            </AnimatePresence>
           </ul>
         </div>
         <div className="flex-1 overflow-y-scroll border-l px-8 py-8">
           <h1 className="h-8 rounded bg-slate-100 text-2xl font-bold" />
           <div className="mt-8 space-y-6">
             {[...Array(9).keys()].map((i) => (
-              <div key={i} className="space-y-2 text-sm">
+              <div
+                key={i}
+                className="space-y-2 text-sm"
+              >
                 <p className="h-4 w-5/6 rounded bg-slate-100" />
                 <p className="h-4 rounded bg-slate-100" />
                 <p className="h-4 w-4/6 rounded bg-slate-100" />
